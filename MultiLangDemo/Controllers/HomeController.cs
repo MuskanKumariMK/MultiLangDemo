5c9c7a6910adc057595655d2caffc506f7c249fa
rajnarayan.gupta <rajnarayan.gupta@pylonix.com>
2026-05-06 15:17:45 +0530

    Add project files.

---
+++ MultiLangDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using MultiLangDemo.Models;
+using System.Diagnostics;
+
+namespace MultiLangDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ControllerMessage =
+                _localizer["ControllerMessage"];
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MultiLangDemo/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Add localization services and configure the resources path
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+// Add MVC services and enable view localization
+builder.Services
+    .AddControllersWithViews()
+    .AddViewLocalization();
+
+// Define the supported cultures for localization
+var supportedCultures = new[]
+{
+    new CultureInfo("en"),
+    new CultureInfo("hi")
+};
+// Configure the request localization options
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+
+var app = builder.Build();
+
+var localizationOptions = app.Services.GetRequiredService<Microsoft.Extensions.Options.IOptions<RequestLocalizationOptions>>();
+// Use the configured request localization options
+app.UseRequestLocalization(
+    localizationOptions.Value);
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
