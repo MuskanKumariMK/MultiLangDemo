799c60ade0c1d60cfdca2164f2ce9e1329a903c4
MuskanKumariMK <muskankumari00999.mk@gmail.com>
2026-05-17 10:55:09 +0530

    Partial View Implemented

---
+++ MultiLangDemo/Views/Shared/_NotificationCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MultiLangDemo.Views.Shared
+{
+    public class _NotificationCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
